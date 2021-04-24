ca67793db1296a57c1792f044b64b7a585081f2d
Aleksei Dmitrenko <64703800+essenceD@users.noreply.github.com>
2021-04-24 12:24:16 +0300

    Add files via upload

--- istagram.py
+++ istagram.py
@@ -12,11 +12,11 @@ import json
 
 class InstagramSpider(scrapy.Spider):
     name = 'instagram'
-    allowed_domains = ['www.instagram.com']
+    allowed_domains = ['www.instagram.com', 'i.instagram.com']
     start_urls = ['https://www.instagram.com/']
     _login_url = '/accounts/login/ajax/'
     _tags_path = '/explore/tags/'
-    _pagination_path = 'https://i.instagram.com/api/v1/tags'
+    _pagination_path = 'https://i.instagram.com/api/v1/tags/'
 
     def __init__(self, login, password, tags, *args, **kwargs):
         self.login = login
@@ -43,35 +43,39 @@ class InstagramSpider(scrapy.Spider):
                     yield response.follow(f'{self._tags_path}{tag}/', callback=self.tag_page_parse)
 
     def tag_page_parse(self, response):
-
-        parse_data = self.get_parse_data(response)
-        yield scrapy.FormRequest(
-            f'{self._pagination_path}/{parse_data["name"]}/sections/',
-            method='POST',
-            callback=self.tag_parse,
-            formdata={
-                'include_persistent': '0',
-                'max_id': parse_data['next_max_id'],
-                'page': f'{parse_data["next_page"]}',
-                'surface': 'grid',
-                'tab': 'recent'
-            }
-        )
-
-        print(1)
+        try:
+            parse_data = self.get_parse_data(response)
+            yield scrapy.FormRequest(
+                f'{self._pagination_path}{parse_data["name"]}/sections/',
+                method='POST',
+                callback=self.tag_page_parse,
+                formdata={
+                    'include_persistent': '0',
+                    'max_id': parse_data['next_max_id'],
+                    'page': f'{parse_data["next_page"]}',
+                    'surface': 'grid',
+                    'tab': 'recent'
+                },
+                headers={'x-csrftoken': parse_data['x-csrftoken']}
+            )
+        except AttributeError:
+            if response.json()['status'] == 'fail':
+                print(1)
 
     def tag_parse(self, response):
         print(2)
 
     def get_parse_data(self, response):
         js_data = self.js_extract(response)
+        print(1)
         return {
             'next_max_id': js_data['entry_data']['TagPage'][0]['data']['recent']['next_max_id'],
             'id': js_data['entry_data']['TagPage'][0]['data']['id'],
             'name': js_data['entry_data']['TagPage'][0]['data']['name'],
             'followers': js_data['entry_data']['TagPage'][0]['data']['media_count'],
             'profile_picture_url': js_data['entry_data']['TagPage'][0]['data']['profile_pic_url'],
-            'next_page': js_data['entry_data']['TagPage'][0]['data']['recent']['next_page']
+            'next_page': js_data['entry_data']['TagPage'][0]['data']['recent']['next_page'],
+            'x-csrftoken': js_data['config']['csrf_token']
         }
 
     def js_extract(self, response):
